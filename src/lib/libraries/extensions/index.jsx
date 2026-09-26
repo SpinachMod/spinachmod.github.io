56e73b6f9dbdf64e3351eb1fa65c757fb1e34e97
~ Gaia ~ <crystalfranks18@hotmail.com>
2026-09-26 11:55:33 -0400

    Change bilup tag from 'bilup' to 'bl'

--- src/lib/libraries/extensions/index.jsx
+++ src/lib/libraries/extensions/index.jsx
@@ -445,7 +445,7 @@ const gallerySourceDisplay = {
         name: 'Bilup Extension Gallery',
         href: 'https://extensions.bilup.org/',
         iconURL: galleryIconBilup,
-        tag: 'bilup'
+        tag: 'bl'
     },
     nitrobolt: {
         name: 'NitroBolt Extension Gallery',
