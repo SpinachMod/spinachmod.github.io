3c1f954318b7d70d03d5d2302afa7d7d3948ff6d
~ Gaia ~ <crystalfranks18@hotmail.com>
2026-09-26 08:29:52 -0400

    Add new brand tags for PotentiaMod and Bilup
    Added new tags for 'PotentiaMod' and 'Bilup' with their respective labels.

--- src/lib/libraries/tw-extension-tags.js
+++ src/lib/libraries/tw-extension-tags.js
@@ -3,6 +3,8 @@ import {APP_NAME} from '../brand';
 // Because there are all brand names, it is unnecessary for them to be translatable.
 export default [
     {tag: 'scratch', intlLabel: 'Scratch'},
-    {tag: 'nb', intlLabel: APP_NAME},
+    {tag: 'pot', intlLabel: 'PotentiaMod'}, //Add PotentiaMod!
+    {tag: 'nb', intlLabel: 'NitroBolt'},
+    {tag: 'bl', intlLabel: 'Bilup'}, //Add Bilup!
     {tag: 'tw', intlLabel: 'TurboWarp'}
 ];
