83b0ae3a7cb8413de25890c1a83c53fc50457379
~ Gaia ~ <crystalfranks18@hotmail.com>
2026-09-26 19:02:07 -0400

    Add GUI menu to the menu reducer

--- src/reducers/menus.js
+++ src/reducers/menus.js
@@ -10,6 +10,7 @@ const MENU_LOGIN = 'loginMenu';
 const MENU_MODE = 'modeMenu';
 const MENU_SETTINGS = 'settingsMenu';
 const MENU_ACCENT = 'accentMenu';
+const MENU_GUI = 'guiMenu';
 const MENU_BLOCKS_THEME = 'blocksThemeMenu';
 const MENU_ERRORS = 'errorsMenu';
 
@@ -55,6 +56,7 @@ const rootMenu = new Menu('root')
         new Menu(MENU_SETTINGS)
             .addChild(new Menu(MENU_LANGUAGE))
             .addChild(new Menu(MENU_ACCENT))
+            .addChild(new Menu(MENU_GUI))
             .addChild(new Menu(MENU_BLOCKS_THEME))
     )
     .addChild(new Menu(MENU_FILE))
@@ -75,6 +77,7 @@ const initialState = {
     [MENU_MODE]: false,
     [MENU_SETTINGS]: false,
     [MENU_ACCENT]: false,
+    [MENU_GUI]: false,
     [MENU_BLOCKS_THEME]: false,
     [MENU_ERRORS]: false
 };
@@ -152,6 +155,10 @@ const openAccentMenu = () => openMenu(MENU_ACCENT);
 const closeAccentMenu = () => closeMenu(MENU_ACCENT);
 const accentMenuOpen = state => state.scratchGui.menus[MENU_ACCENT];
 
+const openGuiMenu = () => openMenu(MENU_GUI);
+const closeGuiMenu = () => closeMenu(MENU_GUI);
+const GuiMenuOpen = state => state.scratchGui.menus[MENU_GUI];
+
 const openBlocksThemeMenu = () => openMenu(MENU_BLOCKS_THEME);
 const closeBlocksThemeMenu = () => closeMenu(MENU_BLOCKS_THEME);
 const blocksThemeMenuOpen = state => state.scratchGui.menus[MENU_BLOCKS_THEME];
@@ -190,6 +197,9 @@ export {
     openAccentMenu,
     closeAccentMenu,
     accentMenuOpen,
+	openGuiMenu,
+    closeGuiMenu,
+    GuiMenuOpen,
     openBlocksThemeMenu,
     closeBlocksThemeMenu,
     blocksThemeMenuOpen,
