ea77e2caf998629d369407dca74d4b30377e42d5
~ Gaia ~ <crystalfranks18@hotmail.com>
2026-09-26 08:57:07 -0400

    Add APP_NAME label for 'sm' tag in tw-extension-tags.js

--- src/lib/libraries/tw-extension-tags.js
+++ src/lib/libraries/tw-extension-tags.js
@@ -3,6 +3,7 @@ import {APP_NAME} from '../brand';
 // Because there are all brand names, it is unnecessary for them to be translatable.
 export default [
     {tag: 'scratch', intlLabel: 'Scratch'},
+    {tag: 'sm', intlLabel: APP_NAME},
     {tag: 'pot', intlLabel: 'PotentiaMod'}, //Add PotentiaMod!
     {tag: 'nb', intlLabel: 'NitroBolt'},
     {tag: 'bl', intlLabel: 'Bilup'}, //Add Bilup!
